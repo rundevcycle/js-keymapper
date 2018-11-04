0ccd5334c34210888abdfe6f87041be2be7c0ab4
Andy <andy@rundevcycle.com>
2018-11-04 19:19:15 -0400

    Clean up JSKeyMapper for better encapsulation.

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
-var JSKeyMapper = {
-    _actionList: {},
+// Make JSKeyMapper a module.
+var JSKeyMapper = function(menuDiv) {
+    var _actionList = {};
+    var _menuDiv = menuDiv;
 
-    register: function(keyAction) {
+    function register(keyAction) {
         if (keyAction instanceof KeyAction) {
             console.log(`Registering action '${keyAction.name}' with key '${keyAction.key}'.`);
-            this._actionList[keyAction.key] = keyAction;
+            _actionList[keyAction.key] = keyAction;
         } else {
             console.warn("Must pass a KeyAction object to be registered.");
         }
-    },
+    };
 
-    unregister: function(key) {
-        if (key in this._actionList) {
-            delete this._actionList[key];
+    function unregister(key) {
+        if (key in _actionList) {
+            delete _actionList[key];
         }
-    },
-    draw: function() {
-        var menu = $("#key-menu");
+    };
+
+    function unregisterAll() {
+        for (var key in _actionList) {
+            delete _actionList[key];
+        }
+    };
+
+    function draw() {
+        var menu = $(menuDiv);
         menu.empty();
-        for (var key in this._actionList) {
-             menu.append(this._actionList[key].draw());
+        for (var key in _actionList) {
+             menu.append(_actionList[key].draw());
         }
-    },
+    };
     
-    monitorKeys: function() {
+    function monitorKeystrokes() {
         $(document).keydown((ev) => {
             console.log("Key down: " + ev.key);
-            if (ev.key in this._actionList) {
-                this._actionList[ev.key].func();
+            if (ev.key in _actionList) {
+                _actionList[ev.key].func();
                 ev.preventDefault();
             }
         });
-    },
+    };
 
+    return {
+        register: register,
+        unregister: unregister,
+        unregisterAll: unregisterAll,
+        draw: draw,
+        monitorKeystrokes: monitorKeystrokes
+    };
 };
 
 
@@ -58,78 +74,89 @@ class KeyAction {
 
 
 
-
+// Here's an example of overridding the Function keys to change colours 
+// and move the box around.
 $(document).ready(() => {
-    JSKeyMapper.register(new KeyAction("F1", "Red", "button",
+    var mapMenu = new JSKeyMapper('#key-menu');
+
+    mapMenu.register(new KeyAction("F1", "Red", "button",
         function() {
             $("#centerSquare").css("background-color", "red");
         })
     );
-    JSKeyMapper.register(new KeyAction("F2", "Orange", "button",
+    mapMenu.register(new KeyAction("F2", "Orange", "button",
         function() {
             $("#centerSquare").css("background-color", "orange");
         })
     );
-    JSKeyMapper.register(new KeyAction("F3", "Yellow", "button",
+    mapMenu.register(new KeyAction("F3", "Yellow", "button",
         function() {
             $("#centerSquare").css("background-color", "yellow");
         })
     );
-    JSKeyMapper.register(new KeyAction("F4", "Green", "button",
+    mapMenu.register(new KeyAction("F4", "Green", "button",
         function() {
             $("#centerSquare").css("background-color", "green");
         })
     );
-    JSKeyMapper.register(new KeyAction("F5", "Blue", "button",
+    mapMenu.register(new KeyAction("F5", "Blue", "button",
         function() {
             $("#centerSquare").css("background-color", "blue");
         })
     );
-    JSKeyMapper.register(new KeyAction("F6", "Violet", "button",
+    mapMenu.register(new KeyAction("F6", "Violet", "button",
         function() {
             $("#centerSquare").css("background-color", "purple");
         })
     );
-    JSKeyMapper.register(new KeyAction("F7", "Move Left", "button",
+    mapMenu.register(new KeyAction("F7", "Move Left", "button",
         function() {
             var el = $("#centerSquare");
             el.css("left", `${el.position().left - 5}px`);
         })
     );
-    JSKeyMapper.register(new KeyAction("F8", "Move Right", "button",
+    mapMenu.register(new KeyAction("F8", "Move Right", "button",
         function() {
             var el = $("#centerSquare");
             el.css("left", `${el.position().left + 5}px`);
         })
     );
-    JSKeyMapper.register(new KeyAction("F9", "Move Up", "button",
+    mapMenu.register(new KeyAction("F9", "Move Up", "button",
         function() {
             var el = $("#centerSquare");
             el.css("top", `${el.position().top - 5}px`);
         })
     );
-    JSKeyMapper.register(new KeyAction("F10", "Move Down", "button",
+    mapMenu.register(new KeyAction("F10", "Move Down", "button",
         function() {
             var el = $("#centerSquare");
             el.css("top", `${el.position().top + 5}px`);
         })
     );
-    JSKeyMapper.register(new KeyAction("F11", "Bigger", "button",
+    mapMenu.register(new KeyAction("F11", "Bigger", "button",
         function() {
             var el = $("#centerSquare");
-            el.css("width", `${el.width() + 5}px`);
-            el.css("height", `${el.height() + 5}px`);
+            el.width(el.width() + 10);
+            el.height(el.height() + 10);
         })
     );
-    JSKeyMapper.register(new KeyAction("F12", "Smaller", "button",
+    mapMenu.register(new KeyAction("F12", "Smaller", "button",
         function() {
             var el = $("#centerSquare");
-            el.css("width", `${el.width() - 5}px`);
-            el.css("height", `${el.height() - 5}px`);
+            el.width(el.width() - 10);
+            el.height(el.height() - 10);
         })
     );
-    JSKeyMapper.draw();
-    JSKeyMapper.monitorKeys();
+    mapMenu.register(new KeyAction("b", "Random Background", "button",
+        function() {
+            var red = Math.random() * 255;
+            var green = Math.random() * 255;
+            var blue = Math.random() * 255;
+            $("body").css("background-color", `rgb(${red}, ${green}, ${blue})`);
+        }));
+
+    mapMenu.draw();
+    mapMenu.monitorKeystrokes();
 });
 
 
